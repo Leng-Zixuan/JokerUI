9779fdfb40f58165cbebdbdc6a87d8e8fb76f661
Leng <jyx13164683568@163.com>
2024-03-09 21:15:27 +0800

    更改storybook配置和测试组件

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,7 +1,7 @@
 const config = {
   stories: [
-    "../stories/**/*.mdx",
-    "../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)",
+    "../docs/**/*.mdx",
+    "../components/**/*.stories.@(js|jsx|mjs|ts|tsx)",
   ],
   addons: [
     "@storybook/addon-links",
