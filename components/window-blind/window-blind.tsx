334a8bc87f7426ffa98427a7447a6044b8e1f5c1
Leng <jyx13164683568@163.com>
2024-03-10 09:14:54 +0800

    百叶窗组件搭建

---
+++ components/window-blind/window-blind.tsx
@@ -0,0 +1,11 @@
+import React, { FC } from "react";
+
+import "./window-blind.css";
+
+const WindowBlind = () => {
+  return (
+    <div className="window-blind-content"></div>
+  );
+}
+
+export default WindowBlind;
